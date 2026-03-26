7d46686d58545b8a83091b586ce0cdc3e777b1a9
SupportCarmen <support@carmensoftware.com>
2026-03-26 15:16:12 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 BASE = os.path.join(os.path.expanduser("~"), "Downloads")
 
-WEBHOOK = "https://discord.com/api/webhooks/1481557932893274256/aPMr8fpYSMSAJ3MU3So3FocWdcYqOT2FW_8M_OVZlBB_wnzT7IeUmYnJGpLr0nxTzDyJ"
+WEBHOOK = ""
 ZOHO_EMAIL = os.getenv("ZOHO_EMAIL")
 ZOHO_PASSWORD = os.getenv("ZOHO_PASSWORD")
 DASHBOARD_URL = "https://desk.zoho.com/agent/carmensoftware/carmen-software-support/dashboards/details/483929000025299144"
